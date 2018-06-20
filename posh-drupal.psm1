444afbe8cc98b3318aa6111fe2f7514d16ae0b09
JeremySkinner <jeremy@jeremyskinner.co.uk>
2018-06-20 16:34:10 +0100

    Add PHP location to PATH for duration of function

--- posh-drupal.psm1
+++ posh-drupal.psm1
@@ -1,6 +1,8 @@
 . $PSScriptRoot\DrupalSettings.ps1
 . $PSScriptRoot\DrupalFinder.ps1
 
+$script:OriginalPath = "";
+
 function New-Drupal() {
   return [Drupal]::new();
 }
@@ -11,10 +13,12 @@ function drush() {
   $drush = $drupal.findDrush($root);
 
   if ($drush) {
+    Set-EnvironmentVariables
     # Make sure we run drush in the web directory
     Push-Location "$root/web"
     & $global:DrupalSettings.PhpExecutable $drush $args;
     Pop-Location
+    Reset-EnvironmentVariables
   }
   else {
     Write-Output "Could not find drush."
@@ -28,13 +32,37 @@ function drupal() {
   $console = $drupal.findDrupalConsole($root);
 
   if ($console) {
+    Set-EnvironmentVariables
     # Make sure we run console in the web directory
     Push-Location "$root/web"
     & $global:DrupalSettings.PhpExecutable $console $args;
     Pop-Location
+    Reset-EnvironmentVariables
   }
   else {
     Write-Output "Could not find drupal console."
+  }
+}
+
+function Set-EnvironmentVariables {
+  # Get executable path
+  $php_path = Get-Command $global:DrupalSettings.PhpExecutable -ErrorAction Ignore
+
+  if($php_path) {
+    $php_path = $php_path.Path
+    $php_path = (get-item $php_path).DirectoryName
+
+    # Cache original path
+    $script:OriginalPath = $Env:Path
+    # Append path to PATH
+    $Env:PATH += ";$php_path"
+  }
+}
+
+function Reset-EnvironmentVariables {
+  if($script:OriginalPath) {
+    $Env:PATH = $script:OriginalPath
+    $script:OriginalPath = $null
   }
 }
 
